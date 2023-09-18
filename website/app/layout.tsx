457ec18a15cf93b1166242f325b8e8ffaaae4473
Lee <leesheppard2404@gmail.com>
2023-09-18 16:42:06 +0200

    fix(updated website language to Swedish)

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="sv">
       <body className={poppins.className}>{children}</body>
     </html>
   );
