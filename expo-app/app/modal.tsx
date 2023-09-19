0b210e8116c650ef1fbb22aa0f9fc81a42c0f90a
Lee <leesheppard2404@gmail.com>
2023-09-19 13:57:51 +0200

    refactor(amended model to mimic app)
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- expo-app/app/modal.tsx
+++ expo-app/app/modal.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +8,15 @@ import { Text, View } from '../components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+      <Text style={styles.title}>Inside Modal.tsx</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Link href="/" style={styles.link}>
+        <Text style={styles.linkText}>Logout</Text>
+      </Link>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
@@ -31,5 +38,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+  linkText: {
+    fontSize: 14,
+    color: '#2e78b7',
   },
 });
