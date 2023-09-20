e64e574c95b9974e31412cddcd7e49eb3c86953a
Lee <leesheppard2404@gmail.com>
2023-09-20 15:38:29 +0200

    feat(added fake news data)
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

---
+++ expo-app/fakeData/news.ts
@@ -0,0 +1,41 @@
+export const news = [
+  {
+    id: 'asdfasdfasdfw',
+    author: 'Vaktmästare Persson',
+    header: 'Brandsläckare!',
+    intro: 'Idag hade vi en incident med en brandsläckare.',
+    body: '## Information om brandsläckarincidenten\n\nHej, idag vid lunchtid utlöste en elev av misstag en pulverbrandsläckare i kapprummet. En del pulver yrde runt i rummet och under saneringen fick eleverna i angränsande klassrum vara i aulan istället för klassrummet.\n\nFlera elever var på plats i hallen när detta inträffade men utrymdes kort därefter. Pulvret är INTE hälsovådligt men kan ge upphov till halsirritation vid inandning.\n\nJag har pratat med berörda elever om det inträffade och uppmanat dem att ta hem kläder och tillhörigheter som fanns i kapprummet eftersom de troligen blivit dammiga. Vi rekommenderar att ni tvättar eller vädrar dessa.',
+    imageUrl: '6607f9b923edb6f85aa4417bab43c0f8.jpg',
+    fullImageUrl:
+      'https://cdn.breakit.se/assets/article/6607f9b923edb6f85aa4417bab43c0f8.jpg?d=980x500',
+    imageAltText: 'Nyhetsbild. Bildtext ej tillgänglig.',
+    published: 'dates go here',
+    modified: 'dates go here',
+  },
+  {
+    id: 'asdfabbuasdfs',
+    author: 'Ada L.',
+    header: 'Bygg din egen app',
+    intro: 'Denna vecka bygger vi appar!',
+    body: '## Appar med öppen data \n\nDenna vecka har vi förmånen att få besök av några föräldrar som visar hur vi enkelt kan skapa appar som visar information ifrån öppna datakällor.\n\nEn fantastisk möjlighet att lära oss hur digitalisering skapar nya möjligheter i såväl skolan som arbetslivet.',
+    imageUrl: '6607f9b923edb6f85aa4417bab43c0f8.jpg',
+    fullImageUrl:
+      'https://live.staticflickr.com/4063/4369776892_5cd42d27ba.jpg',
+    imageAltText: 'Nyhetsbild. Bildtext ej tillgänglig.',
+    published: 'dates go here',
+    modified: 'dates go here',
+  },
+  {
+    id: 'asdfasdfasdfs',
+    author: 'Magister Svensson',
+    header: 'Läxor vecka 6.',
+    intro: 'Alla elever måste göra sina läxor!',
+    body: '## Läxor vecka 6 \n\nFöljande läxor är obligatoriska:\n\n- Antikens historia\n- Svenska stormaktstiden\n- Statistik A\n- Flerdimensionell analys, del 1',
+    imageUrl: '6607f9b923edb6f85aa4417bab43c0f8.jpg',
+    fullImageUrl:
+      'https://www.mitti.se/_internal/cimg!0/ejf8efxee735ymm8tm40q3hhkl36sdt.jpeg',
+    imageAltText: 'Nyhetsbild. Bildtext ej tillgänglig.',
+    published: 'dates go here',
+    modified: 'dates go here',
+  },
+];
