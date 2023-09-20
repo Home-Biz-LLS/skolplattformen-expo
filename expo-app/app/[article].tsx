bf72e1208b0c8bc762f61eb123af27ca302fc288
Lee <leesheppard2404@gmail.com>
2023-09-20 16:18:08 +0200

    Feat(added template data of news article)
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- expo-app/app/[article].tsx
+++ expo-app/app/[article].tsx
@@ -2,28 +2,16 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { Stack } from 'expo-router';
 
-type NewsArticleProps = {
-  article: {
-    id: string;
-    author: string;
-    header: string;
-    intro: string;
-    body: string;
-    imageUrl: string;
-    fullImageUrl: string;
-    imageAltText: string;
-    published: string;
-    modified: string;
-  };
-};
-
-export default function NewsArticle({ article }: NewsArticleProps) {
-  console.log(article);
+export default function NewsArticle() {
   return (
     <>
       <Stack.Screen options={{ title: 'News' }} />
       <View>
-        <Text>Hello there!</Text>
+        <Text>(Header goes here)</Text>
+        <Text>(Published)</Text>
+        <Text>(Updated)</Text>
+        <Text>(Body)</Text>
+        <Text>(Img goes here)</Text>
       </View>
     </>
   );
