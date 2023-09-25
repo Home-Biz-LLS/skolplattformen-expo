bac3f59a9dc96f134b135f2730ecd419606e0430
Lee <leesheppard2404@gmail.com>
2023-09-25 09:41:57 +0200

    Trying a different method
    Co-authored-by: Sebastian Palmqvist <PalmN72@users.noreply.github.com>
    Co-authored-by: Lvan Ni <lvan-ni@users.noreply.github.com>

--- app/libs/hooks/src/context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from 'react'
-import { IApiContext } from './types'
-
-export const ApiContext = createContext<IApiContext>({} as IApiContext)
-
-export const useApi = () => useContext(ApiContext)
--- app/libs/hooks/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './provider';
-export * from './context';
-export * from './hooks';
-export * from './types';
